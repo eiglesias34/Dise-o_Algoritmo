64b4d18d020f1a35932275bb6a552cf2326d25ab
Enrique <eiglesias34@gmail.com>
2017-03-28 01:20:56 -0430

    Falta repite-ciclo y declarar las variables globales

--- Proyecto_2/codigo/func_bb.cpp
+++ Proyecto_2/codigo/func_bb.cpp
@@ -12,14 +12,12 @@
 */
 
 
-#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <deque>
 #include <queue>
-#include <algorithm>
 
 #include <time.h>
 #include <limits.h>
@@ -37,26 +35,59 @@ struct Solucion
 	string camino;
 };
 
-deque<Arista> obtener_lista_de_sucesores (Vertice v, deque<Arista>& aristas){
+bool hay_ciclo (struct Arista lado, struct Solucion solucion){
+	for (int i = 0; i < solucion.camino.length(); ++i)
+	{
+		if (lado.nodo2 == atoi(solucion.camino.substr(i,1).c_str()))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool beneficio_negativo (struct Arista lado, struct Solucion solucion, deque<Arista>& aristas){
+	int count = lado.costo - lado.beneficio;
+	for (int i = solucion.camino.length()-1; 0 < i; --i)
+	{
+		Arista a = extraer_arista(aristas, atoi(solucion.camino.substr(i-1,1).c_str()), atoi(solucion.camino.substr(i,1).c_str()));
+		count = count + (a.costo - a.beneficio);
+		if (atoi(solucion.camino.substr(i-1,1).c_str()) == lado.nodo2)
+		{
+			break;
+		}
+	}
+	return count < 0;
+}
+
+bool ciclo_negativo (struct Arista lado, struct Solucion solucion, deque<Arista>& aristas){
+	if ((hay_ciclo(lado, solucion)) and beneficio_negativo(lado, solucion, aristas))
+	{
+		return true;
+	}
+	return false;
+}
+
+deque<Arista> obtener_lista_de_sucesores (AdjListNode v, deque<Arista>& aristas){
 
 	deque<Arista> sucesores;
 	struct AdjListNode* next = v.next;
 	while (next != NULL){
-		Arista lado = extraer_arista(aristas, v.id, next.id);
+		Arista lado = extraer_arista(aristas, v.id, next->id);
 		sucesores.push_front(lado);
-		sucesores.push_front(crear_arista(v.id, next.id, lado.costo, 0));
+		sucesores.push_front(crear_arista(v.id, next->id, lado.costo, 0));
 		next = v.next;
 	}
-	return sucesores
+	return sucesores;
 }
 
 int arista_pertenece (struct Arista lado, struct Solucion solucion){
 
 	int count = 0;
 
-	for (int i = 0; i < strlen(solucion); ++i)
+	for (int i = 0; i < solucion.camino.length(); ++i)
 	{
-		if (((lado.nodo1 == atoi(solucion[i].c_str()))) and ((lado.nodo2 == atoi(solucion[i+1].c_str()))))
+		if (((lado.nodo1 == atoi(solucion.camino.substr(i,1).c_str()))) and ((lado.nodo2 == atoi(solucion.camino.substr(i+1,1).c_str()))))
 		{
 			count++;
 		}
@@ -70,13 +101,15 @@ bool esta_lado_en_solparcial(struct Arista lado, struct Solucion solucion){
 	{
 		return false;
 	}
-	elif (arista_pertenece(lado, solucion) == 1){
+	else if (arista_pertenece(lado, solucion) == 1){
 		if (lado.beneficio == 0)
 		{
 			return false;
 		}else{
 			return true;
 		}
+	}else{
+		return true;
 	}
 }
 
